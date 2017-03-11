94d099641cae95724045122de93c826fe054c919
Teddy Wing <git@teddywing.com>
2017-03-11 10:49:45 +0100

    Get login working
    Fill in the `Login` function to actually log in to Time Task. Pass
    credentials in via test command flags.
    
    Referenced https://gist.github.com/varver/f327ef9087ebf76aa4c4 for the
    cookie setup.

--- timetask/http.go
+++ timetask/http.go
@@ -1,11 +1,33 @@
 package timetask
 
 import (
-	// "mime/multipart"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+
+	"golang.org/x/net/publicsuffix"
 )
 
-func Login() (resp *http.Response, err error) {
-	resp, err = http.Get("https://duckduckgo.com")
+func Login(username, password string) (resp *http.Response, err error) {
+	cookies, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
+	if err != nil {
+		return nil, err
+	}
+
+	client := http.Client{Jar: cookies}
+	resp, err = client.PostForm(
+		"https://af83.timetask.com/index.php",
+		url.Values{
+			"module":     {"people"},
+			"action":     {"loginsubmit"},
+			"f_url":      {"/"},
+			"f_username": {username},
+			"f_password": {password},
+		},
+	)
+	if err != nil {
+		return resp, err
+	}
+
 	return resp, err
 }
--- timetask/http_test.go
+++ timetask/http_test.go
@@ -1,12 +1,22 @@
 package timetask
 
 import (
+	"flag"
 	"io/ioutil"
 	"testing"
 )
 
+var username, password string
+
+func init() {
+	flag.StringVar(&username, "username", "", "Username")
+	flag.StringVar(&password, "password", "", "Password")
+	flag.Parse()
+
+}
+
 func TestLogin(t *testing.T) {
-	response, _ := Login()
+	response, _ := Login(username, password)
 	defer response.Body.Close()
 	body, _ := ioutil.ReadAll(response.Body)
 	t.Log(response)
