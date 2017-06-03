f017c462593496efbc0810ec2603da49a2e3d9d8
Teddy Wing <git@teddywing.com>
2017-06-03 13:02:03 +0200

    Add `NewTimeEntry()`
    A new function that creates a `TimeEntry` build from a `Profile`,
    `Project`, and a few other parameters. Makes it a bit easier to create
    `TimeEntry`ies.
    
    Additionally, add a `PersonID` field to `TimeEntry` so we can pass it
    around as a self-contained thing without worrying about having to pass
    a `Profile` along with it.

--- timetask/time_entry.go
+++ timetask/time_entry.go
@@ -3,6 +3,7 @@ package timetask
 import "time"
 
 type TimeEntry struct {
+	PersonID    int
 	Client      int
 	Project     int
 	Module      int
@@ -12,4 +13,25 @@ type TimeEntry struct {
 	Time        int
 	Billable    bool
 	Description string
+}
+
+func NewTimeEntry(
+	profile Profile,
+	project Project,
+	date time.Time,
+	time int,
+	description string,
+) TimeEntry {
+	return TimeEntry{
+		PersonID: profile.PersonID,
+		Client: project.Client,
+		Project: project.Project,
+		Module: project.Module,
+		Task: project.Task,
+		WorkType: project.WorkType,
+		Date: date,
+		Time: time,
+		Billable: project.Billable,
+		Description: description,
+	}
 }
