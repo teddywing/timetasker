c113de965dea24c44023a9aabcff7f8ee1f0caa0
Teddy Wing <git@teddywing.com>
2017-06-04 00:05:39 +0200

    Add module.go for parsing Module XML
    A new function that parses the XML returned by the `RequestModules()`
    function.
    
    It provides a `Module` type that allows us to interact with modules more
    easily in code.
    
    The `ParseXML()` function will take an XML string and return a slice of
    `Module`s.
    
    Added a test just to facilitate development. Wasn't able to find an easy
    way to compare slices in Go, so just printed the values and checked the
    result visually. Not a useful test for future use, but it served its
    purpose. Eventually it would be nice to find a way to compare structs
    and have a real pass/fail condition.

---
+++ timetask/module_test.go
@@ -0,0 +1,50 @@
+package timetask
+
+import "testing"
+
+const modules_xml = `<?xml version="1.0" encoding="UTF-8" ?>
+<ajax-response>
+	<response type="object" id="ModuleList">
+					<item>
+						<moduleid><![CDATA[55555]]></moduleid>
+						<modulename><![CDATA[R&amp;D]]></modulename>
+						</item>
+					<item>
+						<moduleid><![CDATA[77777]]></moduleid>
+						<modulename><![CDATA[Sprint 1]]></modulename>
+						</item>
+					<item>
+						<moduleid><![CDATA[222222]]></moduleid>
+						<modulename><![CDATA[Sprint 2]]></modulename>
+						</item>
+			</response>
+</ajax-response>`
+
+func TestParseXML(t *testing.T) {
+	modules, err := ParseXML(modules_xml)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_ = []Module{ // wanted
+		Module{
+			ID:   55555,
+			Name: "R&amp;D",
+		},
+		Module{
+			ID:   77777,
+			Name: "Sprint 1",
+		},
+		Module{
+			ID:   222222,
+			Name: "Sprint 2",
+		},
+	}
+
+	// Need a way to compare slices
+	// if modules != wanted {
+	// 	t.Errorf("Module parsing failed. Wanted %+v got %+v", wanted, modules)
+	// }
+
+	t.Logf("%+v\n", modules)
+}
