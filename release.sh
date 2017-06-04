9824ac194096d53191bd54f02f0a64d022a07d0d
Teddy Wing <git@teddywing.com>
2017-06-04 03:24:04 +0200

    Add release.sh script
    Compiles and compresses builds for a number of different platforms,
    placing them all in a dist/ directory for release.

---
+++ release.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+mkdir -p dist
+
+TARGETS="
+	darwin-amd64
+	freebsd-386
+	freebsd-amd64
+	freebsd-arm
+	linux-386
+	linux-amd64
+	linux-arm
+	linux-arm64
+	linux-ppc64
+	openbsd-386
+	openbsd-amd64
+	openbsd-arm
+"
+
+# Remove empty lines and indentation
+TARGETS=$(echo "$TARGETS" | sed -e '/^$/d' -e 's/	//')
+
+for target in $TARGETS; do
+	os=$(echo "$target" | cut -d'-' -f 1 -)
+	arch=$(echo "$target" | cut -d'-' -f 2 -)
+
+	GOOS="$os" \
+	GOARCH="$arch" \
+		go build -v
+
+	tar cjvf "timetasker_${os}_${arch}.tar.bz2" timetasker
+	mv "timetasker_${os}_${arch}.tar.bz2" dist
+done
